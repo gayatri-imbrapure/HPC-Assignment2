c6b6cb6a5455f4b98c3d4574b7c6944ebfa644f5
gayatri-imbrapure <146913542+gayatri-imbrapure@users.noreply.github.com>
2024-09-08 22:59:50 +0530

    Update CalculatePi.c

--- CalculatePi.c
+++ CalculatePi.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <omp.h>
-#include <time.h>
-
+static long num_steps = 1000000;
+double step;
 int main() {
-    long num_points = 10000000;
-    long points_in_circle = 0;
-    double x, y;
-    double pi;
-    srand(time(NULL));
-    #pragma omp parallel private(x, y) reduction(+:points_in_circle)
-    {
-        #pragma omp for
-        for (long i = 0; i < num_points; i++) {
-            x = (double)rand() / RAND_MAX;
-            y = (double)rand() / RAND_MAX;
-            if (x*x + y*y <= 1.0) {
-                points_in_circle++;
-            }
-        }
+    int i;
+    double x, pi, sum = 0.0;
+    step = 1.0 / (double)num_steps;
+    double start_time_seq = omp_get_wtime();  
+    sum = 0.0;
+    for (i = 0; i < num_steps; i++) {
+        x = (i + 0.5) * step;
+        sum += 4.0 / (1.0 + x * x);
     }
-    pi = 4.0 * points_in_circle / num_points;
+    pi = step * sum;
+    double end_time_seq = omp_get_wtime();  
+    double Tseq = end_time_seq - start_time_seq;
+    printf("Sequential Execution:\n");
+    printf("Calculated Pi = %.15f\n", pi);
+    printf("Time taken: %f seconds\n\n", Tseq);
+    double start_time_par = omp_get_wtime();  
+    sum = 0.0;
+    #pragma omp parallel for private(x) reduction(+:sum) num_threads(4)
+    for (i = 0; i < num_steps; i++) {
+        x = (i + 0.5) * step;
+        sum += 4.0 / (1.0 + x * x);
+    }
+    pi = step * sum;
+    double end_time_par = omp_get_wtime();  
+    double Tparallel = end_time_par - start_time_par;
+    printf("Parallel Execution:\n");
+    printf("Calculated Pi = %.15f\n", pi);
+    printf("Time taken: %f seconds\n", Tparallel);
 
-    printf("Estimated value of Pi: %f\n", pi);
+    double speedup = Tseq / Tparallel;
+    printf("\nSpeedup: %f\n", speedup);
 
     return 0;
 }
